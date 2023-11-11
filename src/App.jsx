fa0c809015760a91f4e7b4fb530e91e8dba72005
apurslow <aaronp26@uw.edu>
2023-11-11 14:16:48 -0800

    updated theatre package, fixed PlaneBufferGeometry

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,60 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { ScrollControls, useScroll } from "@react-three/drei";
+import { getProject, val } from "@theatre/core";
+import { SheetProvider, PerspectiveCamera, useCurrentSheet } from "@theatre/r3f";
 import FantasyBook from "./modelComponents/FantasyBook";
-import { OrbitControls } from "@react-three/drei";
+
 
 
 function App() {
 
+  const sheet = getProject('Fly Through').sheet('Scene')
+
 
   return (
-    <Canvas camera={{ fov: 75, near: 1, far: 1000, position: [7, 15, 60] }}>
-      <color attach='background' args={['skyblue']} />
-      <OrbitControls />
-      <ambientLight intensity={0.75} />
-      <FantasyBook />
-
+    <Canvas gl={{ preserveDrawingBuffer: true }}>
+      <ScrollControls pages={5}>
+        <SheetProvider sheet={sheet}>
+          <Scene />
+        </SheetProvider>
+      </ScrollControls>
     </Canvas>
   )
 }
 
 export default App
+
+
+///
+///
+function Scene() {
+
+  const sheet = useCurrentSheet()
+  const scroll = useScroll()
+
+  //our callback will run on every animation frame
+  useFrame(() => {
+
+    //the length of our sequence
+    const sequenceLength = val(sheet.sequence.pointer.length)
+
+    //update the "position" of trhe playhead in the  sequence as a fraction of the whole length
+    sheet.sequence.position = scroll.offset * sequenceLength
+
+  })
+  return (
+    <>
+      <color attach='background' args={['skyblue']} />
+      <ambientLight intensity={0.75} />
+      <FantasyBook />
+      <PerspectiveCamera
+        theatreKey="Camera"
+        makeDefault
+        position={[0, 0, 5]}
+        fov={90}
+        near={0.1}
+        far={70}
+      />
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import studio from '@theatre/studio'
+import extension from '@theatre/r3f/dist/extension'
+
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+studio.extend(extension);
+studio.initialize();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback = {null}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
 )
